35efcbd1eb54fb77371d564f9b2805c90e4d86dd
Bernd Porr <mail@berndporr.me.uk>
2022-02-23 11:46:36 +0000

    Added client

--- docs/images.pl
+++ docs/images.pl
@@ -7,5 +7,9 @@ $cached_env_img{$key} = q|<IMG STYLE=""
  SRC="|."$dir".q|img1.svg"
  ALT="\includegraphics[width=\textwidth]{signals-timings}">|; 
 
+$key = q/to;MSF=1.6;LFS=12;AAT/;
+$cached_env_img{$key} = q|<IMG STYLE="height: 1.01ex; vertical-align: -0.09ex; " SRC="|."$dir".q|img2.svg"
+ ALT="$\to$">|; 
+
 1;
 
